1db050b2239cde4e0ec0d08356e89d45255139db
Szymon Nowak <now.szymon@gmail.com>
2017-12-06 17:27:11 +0100

    v0.13b
    HeatmapMonthly, SpreadChart and CorrelationChart added

--- alyssa.py
+++ alyssa.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.offsetbox import AnchoredText
 
 """
-AlysSA v0.11b
+AlysSA v0.13b
 
 NextDayReturn(Data, DataColumn, PreviousDay, MoreLess='More', Days=1): Calculate the day return on the day after daily return of PreviousDay value occured.
 BarsInaRow(Data, DataColumn): How many times there was a X number of down/up candles in a row.
@@ -14,6 +14,10 @@ SeasonalityDaily(DataFrame, Column): Calculate mean of daily returns and percent
 Rebase(DataFrame): Calucalte percent changes to the first value in a column.
 Fed_rates(): Load pickled df with details of FED funds rates 
 Plot_X_values_on_Y_dates(df, Column, Dates, fred_api, Range_start=-20, Range_end=60,Title='',PlotAll=False): Plot min and max boundaries for the average price action on selected days.	
+Bubbles(): Load pickled df with price bubbles data
+HeatmapMonthly(df, DateColumn, CloseColumn, Day=False): Heatmap of daily/monthly returns.
+CorrelationChart(df,col1,col2,window=20, CorrPrices=False):	Price/regression/correlation chart with basic statistics.
+SpreadChart(df,col1,col2): Spread chart.
 """
 
 
@@ -74,7 +78,7 @@ def RenameMonths(DataFrame):
 	DataFrame.rename(index={1.0:'January', 2.0:'February', 3.0:'March', 4.0:'April', 5.0:'May',6.0:'June',7.0:'July',8.0:'August',9.0:'September', 10.0:'October',11.0:'November',12.0:'December'}, inplace=True)
 	return DataFrame
 	
-def DownloadFromStooq(Symbol, Interval, Part = False, Date_from = '2000-01-01', Date_to = '2001-01-01', Open=False, High=False, Low=False, Close=True, Volume=False):
+def DownloadFromStooq(Symbol, Interval, Part = False, Date_from = '2000-01-01', Date_to = '2100-01-01', Open=False, High=False, Low=False, Close=True, Volume=False):
 	"""
 	Download data from stooq.plot
 	"""
@@ -177,7 +181,7 @@ def BarsInaRow(Data, DataColumn):
 	res = pd.DataFrame(final['UP'].value_counts())
 	res.sort_index(inplace=True)
 
-	anchored_text = AnchoredText('                AlysSA v0.11b\n  Algorithmic\'n\'Statistical Analysis\nSzymon Nowak (www.1do1.biz.pl)', loc=1)
+	anchored_text = AnchoredText('                AlysSA v0.13b\n  Algorithmic\'n\'Statistical Analysis\nSzymon Nowak (www.1do1.biz.pl)', loc=1)
 		
 	print(final['UP'].value_counts().sort_index())
 	print()
@@ -186,7 +190,7 @@ def BarsInaRow(Data, DataColumn):
 	ax.title.set_size(20)
 	ax.add_artist(anchored_text)
 	
-def SeasonalityPattern(DataFrame, Column):
+def SeasonalityPattern(DataFrame, Column, title=''):
 	"""
 	Plot chart of seasonality pattern with the actual year data. Always delete automatically first year.
 	
@@ -214,9 +218,11 @@ def SeasonalityPattern(DataFrame, Column):
 		plotdata.loc[i,'Projection'] = path.iloc[i,:-1].mean()					
 	plotdata[listofyears[-1]] = path[listofyears[-1]]
 	
+	plotdata = plotdata[:-5]
 
-	anchored_text = AnchoredText('                AlysSA v0.11b\n  Algorithmic\'n\'Statistical Analysis\n       www.szymonnowak.com', loc=4)	
-	ax = plotdata.plot(y=['Projection',listofyears[-1]], secondary_y=['Projection'],figsize=(12,8), title='Seasonality Pattern Projection (r = {}%)'.format(round(plotdata.corr().iloc[0,1]*100)))
+	anchored_text = AnchoredText('                AlysSA v0.13b\n  Algorithmic\'n\'Statistical Analysis\n       www.szymonnowak.com', loc=4)	
+	ax = plotdata.plot(y=['Projection',listofyears[-1]], secondary_y=['Projection'],figsize=(12,8), title='Seasonality Pattern Projection ({} r = {}%)'.format(title,round(plotdata.corr().iloc[0,1]*100)))
+	ax.set_xlabel('Days')
 	ax.add_artist(anchored_text)
 	
 def SeasonalityDaily(DataFrame, Column):
@@ -339,7 +345,7 @@ def Plot_X_values_on_Y_dates(df, Column, Dates, fred_api, Range_start=-20, Range
 	final['zero'] = pd.to_numeric(final['zero'])
 	
 	if PlotAll == False:
-		anchored_text = AnchoredText('                AlysSA v0.11b\n  Algorithmic\'n\'Statistical Analysis\n       www.szymonnowak.com', loc=4)	
+		anchored_text = AnchoredText('                AlysSA v0.13b\n  Algorithmic\'n\'Statistical Analysis\n       www.szymonnowak.com', loc=4)	
 		ax = final.plot(y=['min'],figsize=(12,8), label='min',color='Red',title=Title)
 		final['max'].plot(ax=ax,label='max',color='Green')
 		final['average'].plot(ax=ax,label='average', linewidth=3)
@@ -352,4 +358,151 @@ def Plot_X_values_on_Y_dates(df, Column, Dates, fred_api, Range_start=-20, Range
 		ax.fill_between(final.index,final['max'],final['min'],alpha=0.3,color='Pink')
 		ax.add_artist(anchored_text)
 	else:
-		final.plot(figsize=(12,8),legend=False)
+		anchored_text = AnchoredText('                AlysSA v0.13b\n  Algorithmic\'n\'Statistical Analysis\n       www.szymonnowak.com', loc=4)
+		ax = final.plot(figsize=(12,8),legend=False, title=Title)
+		ax.set_xlabel('Days')
+		ax.set_ylabel('[%]')
+		ax.add_artist(anchored_text)
+		
+def Bubbles():
+
+	import pickle
+	
+	infile = open('data/bubbles','rb')
+	data = pickle.load(infile)
+	infile.close()
+	return(data)
+	
+def HeatmapMonthly(df, DateColumn, CloseColumn, Day=False):
+
+	"""
+	Heatmap of daily/monthly returns.
+	
+	df: pandas DataFrame, DateColumn: column with dates, CloseColumn: column with close prices, Day=False: monthly returns
+	"""
+
+	import seaborn as sns
+	
+	df['Year'] = pd.DatetimeIndex(df[DateColumn]).year
+	df['Month'] = pd.DatetimeIndex(df[DateColumn]).month
+	df['Day'] = pd.DatetimeIndex(df[DateColumn]).weekday
+	
+	df['Return'] = df[CloseColumn].pct_change()*100
+	
+	if Day == False:
+		avg = df[['Month', 'Return']].groupby(['Month'],as_index=False).mean()
+	else:
+		avg = df[['Day', 'Return']].groupby(['Day'],as_index=False).mean()
+		
+	avg['Year'] = 'Avg'
+	new = pd.concat([df,avg])
+	
+	if Day == False:
+		final = new.pivot_table(columns='Month',index='Year',values='Return')
+	else:
+		final = new.pivot_table(columns='Day',index='Year',values='Return')
+		final = final.rename(columns={0:'Monday',1:'Tuesday',2:'Wednesday',3:'Thursday',4:'Friday'})
+		
+	fig, ax = plt.subplots(figsize=(15,12))
+	g = sns.heatmap(final, annot=True, cmap='PiYG')
+	ax.invert_yaxis()	
+
+def CorrelationChart(df,col1,col2,window=20, CorrPrices=False):
+
+	"""
+	Price/regression/correlation chart with basic statistics.
+	
+	df: pandas DataFrame, col1: Column with first instrument data, col2: Column with second instrument data, window: rolling correlation winow, CorrPrices=False: Correllate returns instead of prices
+	"""
+		
+	import seaborn as sns
+	
+	pct = df
+	pct = pct.pct_change()
+	
+	pct['corr'] = pct[col1].rolling(window).corr(pct[col2])
+	last_corr = pct['corr'][-1]*100
+	full_corr = df.corr()
+	full_corr = full_corr.iloc[0,1]*100
+	
+	print('-'*40)
+	print('Returns:')
+	print('-'*40)
+	
+	print(pct.describe())
+
+	print('-'*40)
+	print('Prices:')
+	print('-'*40)	
+	
+	print(df.describe())
+
+	fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=False, figsize=(15,12), gridspec_kw = {'height_ratios':[3, 1]})	
+	ax1.plot(df[col1], color='orange', label='{}'.format(col1))
+	axy = ax1.twinx()
+	axy.plot(df[col2], label='{}'.format(col2))
+	ax2.plot(pct['corr'], linewidth=0.1)
+	
+	ax1.set_title('{} vs. {} (r={}%)'.format(col1,col2,"%.2f" % full_corr), fontsize=15)
+	ax2.set_title('Correlation({}) = {}%'.format(window,"%.2f" % last_corr), fontsize=15)
+	
+	ax1.grid(False)
+	ax1.xaxis.grid(True)
+	axy.grid(False)
+
+	ax1.legend(loc=2)
+	axy.legend()
+
+	ax1.set_ylabel('{}'.format(col1))
+	axy.set_ylabel('{}'.format(col2))
+
+	ax2.fill_between(pct.index.values, 0, pct['corr'], where=pct['corr']>0, interpolate=True, color='green')
+	ax2.fill_between(pct.index.values, 0, pct['corr'], where=pct['corr']<0, interpolate=True, color='red')
+
+	anchored_text = AnchoredText('                AlysSA v0.13b\n  Algorithmic\'n\'Statistical Analysis\n       www.szymonnowak.com', loc=3)
+	ax1.add_artist(anchored_text)
+	
+	if CorrPrices==False:
+		jointplot = sns.jointplot(col1,col2,pct,kind='reg',size=15)
+	else: 
+		jointplot = sns.jointplot(col1,col2,df,kind='reg',size=15)	
+
+def SpreadChart(df,col1,col2):
+
+	"""
+	Spread chart.
+	
+	df: pandas DataFrame, col1: Column with first instrument data, col2: Column with second instrument data
+	"""
+	
+	import seaborn as sns
+
+	df = Rebase(df)
+	
+	df['spread'] = df[col1] - df[col2]
+	
+	fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=False, figsize=(15,12), gridspec_kw = {'height_ratios':[3, 1]})
+	ax1.plot(df[col1],label='{}'.format(col1))
+	ax1.plot(df[col2],label='{}'.format(col2))	
+	ax2.plot(df['spread'], linewidth=0.1)
+
+	ax1.set_title('{} vs. {}'.format(col1,col2),fontsize=15)
+	ax2.set_title('Spread',fontsize=15)
+	
+	ax1.legend(loc=2)
+
+	ax1.grid(False)
+	ax1.xaxis.grid(True)
+	ax2.grid(False)
+	ax2.xaxis.grid(True)
+
+	ax1.fill_between(df.index.values,df['XAUUSD'],df['CA_C.F'],where=df['XAUUSD']>df['CA_C.F'],interpolate=True,color=sns.xkcd_rgb["denim blue"], alpha=0.2)
+	ax1.fill_between(df.index.values,df['XAUUSD'],df['CA_C.F'],where=df['XAUUSD']<df['CA_C.F'],interpolate=True,color=sns.xkcd_rgb["medium green"], alpha=0.2)
+					 
+	ax2.fill_between(df.index.values,0,df['spread'],where=df['spread']>0,interpolate=True,color=sns.xkcd_rgb["denim blue"], alpha=0.7)
+	ax2.fill_between(df.index.values,0,df['spread'],where=df['spread']<0,interpolate=True,color=sns.xkcd_rgb["medium green"], alpha=0.7)
+
+	
+	anchored_text = AnchoredText('                AlysSA v0.13b\n  Algorithmic\'n\'Statistical Analysis\n       www.szymonnowak.com', loc=3)
+	ax1.add_artist(anchored_text)
+	
